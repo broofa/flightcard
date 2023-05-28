81f878c056a921e97f2e6564dad28fc55b613318
Robert Kieffer <robert@broofa.com>
2023-05-28 07:46:57 -0700

    update checkcss dependency

--- index.tsx
+++ index.tsx
@@ -1,4 +1,4 @@
-import checkCSS, { monitorCSS } from 'checkcss';
+import { CheckCSS } from 'checkcss';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -6,8 +6,8 @@ import App from './components/App/App';
 
 // Runtime checks for references to non-existent CSS classes (DEV only)
 if (process.env.NODE_ENV == 'development') {
-  checkCSS();
-  monitorCSS();
+  const checkcss = new CheckCSS();
+  checkcss.scan().watch();
 }
 
 const root = createRoot(document.querySelector('#content') as Element);
