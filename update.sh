d8fd417acad9b4ffe06ff84df5b64da15b9879c2
Robert Kieffer <robert@broofa.com>
2024-07-08 05:28:22 -0700

    update package.json to yarn and esm

--- update.sh
+++
@@ -1,6 +0,0 @@
-npm install parcel@nightly \
-  @parcel/babel-plugin-transform-runtime@nightly \
-  @parcel/packager-raw-url@nightly \
-  @parcel/transformer-sass@nightly \
-  @parcel/transformer-svg-react@nightly \
-  @parcel/transformer-webmanifest@nightly
