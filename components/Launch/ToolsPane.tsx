40a051d861011fcc7ea219bd1f58f3f91c1ec917
Robert Kieffer <robert@broofa.com>
2024-10-16 21:30:33 -0700

    feat: show membership scrape dates

--- components/Launch/ToolsPane.tsx
+++ components/Launch/ToolsPane.tsx
@@ -5,10 +5,34 @@ import useDebounce from '/util/useDebounce';
 
 const { MEMBER_API_ENDPOINT } = process.env;
 
+type MembersMeta = {
+  nar: {
+    queryAccountId: number,
+    queryTimestamp: number,
+    trackingAccountId: number,
+    trackingTimestamp: number,
+    pagination: {
+      currentPage: number,
+      pageSize: number,
+      sortColumn: string,
+      sortDirection: string,
+      totalPages: number,
+      totalResults: number
+    },
+    updatedAt: string
+  },
+  tra: {
+    updatedAt: string,
+    certsFetched: number,
+    lastModified: string
+  }
+}
+
 export function ToolsPane() {
   const [searchText, setSearchText] = React.useState('');
   const [debouncedQuery] = useDebounce(searchText, 500);
   const [members, setMembers] = React.useState<iCert[]>([]);
+  const [membersMeta, setMembersMeta] = React.useState<MembersMeta>();
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     const v = e.target.value;
@@ -38,15 +62,37 @@ export function ToolsPane() {
     setMembers(json.results);
   }
 
+  async function fetchMembersMeta() {
+    const queryURL = new URL(`${MEMBER_API_ENDPOINT!}/members/meta`);
+
+    const res = await fetch(queryURL.toString(), {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const json: MembersMeta = await res.json();
+    setMembersMeta(json);
+  }
+
+
+
   useEffect(() => {
     console.log('debouncedQuery:', debouncedQuery);
     const [lastName, firstName] = searchText.trim().split(/[\W]+/);
 
     if (!lastName || lastName.length < 2) return;
 
+    // TODO: error handling
     fetchMembers({ lastName, firstName });
 
   }, [debouncedQuery]);
+
+  useEffect(() => {
+    // TODO: error handling
+    fetchMembersMeta();
+  }, [])
 
   return (
     <>
@@ -63,6 +109,12 @@ export function ToolsPane() {
           </tr>
         ))}
       </table>
+
+      {membersMeta ? <div className='members-meta'>
+        <div>Last NAR update: {new Date(membersMeta.nar.updatedAt).toDateString()}</div>
+        <div>Last TRA update: {new Date(membersMeta.tra.lastModified).toDateString()}</div>
+      </div> : null
+      }
     </>
   );
 }
